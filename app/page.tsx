6c37b89ad755ed72f1106e5cd1a42c63e153e8ae
KaranGulve4342 <karangulve4342@gmail.com>
2024-02-17 01:24:30 +0530

    commited changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
+import About from "@/components/shared/About";
+import Contact from "@/components/shared/Contact";
 import Hero from "@/components/shared/Hero";
+import Jobs from "@/components/shared/Jobs";
+import Sponsors from "@/components/shared/Sponsors";
+import { prisma } from "@/lib/prisma";
 
-
-const Home = () => {
+const Home = async () => {
+  const jobs = await prisma.jobPosting.findMany({});
   return (
     <>
-      <Hero />
+      <Hero jobs={jobs} />
+      <Jobs />
+      <Sponsors />
+      <About />
+      <Contact />
     </>
-  )
-}
+  );
+};
 
 export default Home;
